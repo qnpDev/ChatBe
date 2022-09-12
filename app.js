381f4da187b4da67a96daac4b3c8f77ff29f7a84
Nguyễn Quí <qnpdev@gmail.com>
2022-09-12 17:18:23 +0700

    fix cors part 2

--- app.js
+++ app.js
@@ -20,21 +20,6 @@ const server = app.listen(process.env.PORT || 9000, () => {
     console.log('qnp | server started!')
 })
 
-app.use( (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-    next()
-})
-
 route(app)
 
 //Index page
